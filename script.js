0cfe463e8711d5c1690956088d9a82fef7eb1e4c
Bojan Rađenović <github@radjenovic.dev>
2024-11-30 00:12:35 +0100

    Add Taylor Swift Page, implement dual slideshow functionality and enhance styling for navigation

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 let slideIndex = 0;
-    showSlides();
-    
-    function showSlides() {
+    showSlides1();
+    showSlides2();
+    function showSlides1() {
       let i;
-      let slides = document.getElementsByClassName("mySlides");
+      let slides = document.getElementsByClassName("mySlides1");
       for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
       }
       slideIndex++;
       if (slideIndex > slides.length) {slideIndex = 1}
       slides[slideIndex-1].style.display = "block";
-      setTimeout(showSlides, 3500); // Change image every 2 seconds
+      setTimeout(showSlides1, 2500); // Change image every 2 seconds
+    }
+    function showSlides2() {
+      let i;
+      let slides = document.getElementsByClassName("mySlides2");
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}
+      slides[slideIndex-1].style.display = "block";
+      setTimeout(showSlides2, 2500); // Change image every 2 seconds
     }
     document.querySelector('.hamburger-menu').addEventListener('click', function() {
       var dropdownMenu = document.querySelector('.dropdown-menu');
