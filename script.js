9e9e2778a6f5d4bd4f5864826933248dcfbfa6dc
Bojan Rađenović <github@radjenovic.dev>
2024-11-30 22:10:11 +0100

    Update navigation links, update navigation bar styling, update hamburger menu, improve JavaScript code

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-let slideIndex = 0;
+document.querySelector('.hamburger-menu').addEventListener('click', function() {
+  var dropdownMenu = document.querySelector('.dropdown-menu');
+  if (dropdownMenu.style.display === 'flex') {
+    dropdownMenu.style.display = 'none';
+  } else {
+    dropdownMenu.style.display = 'flex';
+  }
+});
+let slideIndex1 = 0;
+let slideIndex2 = 0;
+let slides1 = document.getElementsByClassName("mySlides1");
+let slides2 = document.getElementsByClassName("mySlides2");
     showSlides1();
     showSlides2();
     function showSlides1() {
-      let i;
-      let slides = document.getElementsByClassName("mySlides1");
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+      for (i = 0; i < slides1.length; i++) {
+        slides1[i].style.display = "none";
       }
-      slideIndex++;
-      if (slideIndex > slides.length) {slideIndex = 1}
-      slides[slideIndex-1].style.display = "block";
-      setTimeout(showSlides1, 2500); // Change image every 2 seconds
+      slideIndex1++;
+      if (slideIndex1 > slides1.length) {slideIndex1 = 1}
+      slides1[slideIndex1-1].style.display = "block";
+      setTimeout(showSlides1, 2500); // Change image every 2.5 seconds
     }
     function showSlides2() {
-      let i;
-      let slides = document.getElementsByClassName("mySlides2");
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+      if(slides2){
+      for (i = 0; i < slides2.length; i++) {
+        slides2[i].style.display = "none";
       }
-      slideIndex++;
-      if (slideIndex > slides.length) {slideIndex = 1}
-      slides[slideIndex-1].style.display = "block";
-      setTimeout(showSlides2, 2500); // Change image every 2 seconds
+      slideIndex2++;
+      if (slideIndex2 > slides2.length) {slideIndex2 = 1}
+      slides2[slideIndex2-1].style.display = "block";
+      setTimeout(showSlides2, 2500); // Change image every 2.5 seconds
     }
-    document.querySelector('.hamburger-menu').addEventListener('click', function() {
-      var dropdownMenu = document.querySelector('.dropdown-menu');
-      if (dropdownMenu.style.display === 'flex') {
-        dropdownMenu.style.display = 'none';
-      } else {
-        dropdownMenu.style.display = 'flex';
-      }
-    });
+  }
