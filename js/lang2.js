7840fb1e1a0e754576a69712a58b28ba24bcb41b
Bojan Rađenović <bojanradjenovic@protonmail.com>
2024-12-14 00:29:10 +0100

    this should actually work

--- js/lang2.js
+++ js/lang2.js
@@ -1,62 +1,62 @@
 const language = localStorage.getItem('language');
 const currentPage = window.location.pathname;
 if (currentPage.includes('-en') && language === 'serbian') {
-  const serbianPage = currentPage.replace('-en', '');
-  window.location.href = serbianPage;
+  window.location.href = 'index.html';
 }
-if(currentPage === '/-en.html' && language === 'serbian') {
-  window.location.href = '/';
-}
+
 if (!currentPage.includes('-en') && language === 'english') {
-  const englishPage = currentPage.replace(/(\.html)?$/, '-en.html');
-  window.location.href = englishPage;
+  window.location.href = '-en.html';
 }
+
 async function redirectByLocation() {
-    try {
-      const response = await fetch('https://ipwhois.app/json/');
-      const data = await response.json();
-      const country = data.country_code; 
-      const redirectCountries = ['RS', 'ME', 'HR', 'BA']; 
-      if (!redirectCountries.includes(country)) {
-        localStorage.setItem('language', 'english');
-        const englishPage = currentPage.replace(/(\.html)?$/, '-en.html'); 
-        window.location.href = englishPage;
-      } else {
-        localStorage.setItem('language', 'serbian');
-      }
-    } catch (error) {
-      console.error('Error fetching geolocation:', error);
+  try {
+    const response = await fetch('https://ipwhois.app/json/');
+    const data = await response.json();
+    const country = data.country_code; 
+    const redirectCountries = ['RS', 'ME', 'HR', 'BA']; 
+    if (!redirectCountries.includes(country)) {
+      localStorage.setItem('language', 'english');
+      const englishPage = currentPage.replace(/(\.html)?$/, '-en.html'); 
+      window.location.href = englishPage;
+    } else {
+      localStorage.setItem('language', 'serbian');
     }
+  } catch (error) {
+    console.error('Error fetching geolocation:', error);
   }
+}
+
 const languageToggleRs = document.getElementById('language-toggle-rs');
 const languageToggleMobileRs = document.getElementById('language-toggle-mobile-rs');
 
 if (languageToggleRs) {
   languageToggleRs.addEventListener('click', function() {
     localStorage.setItem('language', 'serbian');
-    location.reload();
+    window.location.href = 'index.html';
   });
 }
 
 if (languageToggleMobileRs) {
   languageToggleMobileRs.addEventListener('click', function() {
     localStorage.setItem('language', 'serbian');
-    location.reload();
+    window.location.href = 'index.html';
   });
 }
+
 const languageToggleEn = document.getElementById('language-toggle-en');
 const languageToggleMobileEn = document.getElementById('language-toggle-mobile-en');
 
 if (languageToggleEn) {
   languageToggleEn.addEventListener('click', function() {
     localStorage.setItem('language', 'english');
-    location.reload();
+      window.location.href = '-en.html';
   });
 }
+
 if (languageToggleMobileEn) {
   languageToggleMobileEn.addEventListener('click', function() {
     localStorage.setItem('language', 'english');
-    location.reload();
+      window.location.href = '-en.html';
   });
 }
 
